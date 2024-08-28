aa774c25c7a634bc5c4c8801f7b38c96d934f59f
leejunho <junho767@naver.com>
2024-08-28 19:39:00 +0900

    백엔드 <=> 프론트 구동가능하게

--- Kopmorning/src/main/reactts/src/index.tsx
+++ Kopmorning/src/main/reactts/src/index.tsx
@@ -1,21 +1,22 @@
+// URL 의 이동을 토큰이 존재하냐 안하냐의 여부로 제어할 것이기 때문에
+// 'react-router-dom 의 BrowserRouter 를 App 위에 덮어주었고
+// 그 위에 Context 적용을 위해 AuthContextProvider 를 덮어주었다.
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { AuthContextProvider } from './store/auth-context';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthContextProvider>
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+    </AuthContextProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
