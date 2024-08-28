81399d65b733410f417516c1d1808202d588f4fb
leejunho <junho767@naver.com>
2024-08-28 19:30:28 +0900

    백엔드 <=> 프론트 구동가능하게

---
+++ Kopmorning/src/main/reactts/src/auth/auth-action.tsx
@@ -0,0 +1,96 @@
+import { GET, POST }  from "./fetch-auth-action";
+
+// 토큰 생성 함수, auth-action 에서만 사용 가능
+const createTokenHeader = (token:string) => {
+  return {
+    headers: {
+      'Authorization': 'Bearer ' + token
+    }
+  }
+}
+// 토큰의 만료시간을 계싼하는 함수, auth-action 에서만 사용 가능
+const calculateRemainingTime = (expirationTime:number) => {
+  const currentTime = new Date().getTime();
+  const adjExpirationTime = new Date(expirationTime).getTime();
+  const remainingDuration = adjExpirationTime - currentTime;
+  return remainingDuration;
+};
+
+// 토큰값과 만료시간을 부여 받으면 받은 값을 LocalStorage 에 저장하는 함수, 이 후 남은 시간 반환
+export const loginTokenHandler = (token:string, expirationTime:number) => {
+  localStorage.setItem('token', token);
+  localStorage.setItem('expirationTime', String(expirationTime));
+  const remainingTime = calculateRemainingTime(expirationTime);
+  return remainingTime;
+}
+
+// localStorage 에 토큰이 존재하는 지 검색하는 함수, 존재한다면 만료까지 남은 시간과 토큰값을 같이 반환
+// 만약 만료 시간이 1초 아래로 남았다면 자동으로 토큰을 삭제 함.
+export const retrieveStoredToken = () => {
+  const storedToken = localStorage.getItem('token');
+  const storedExpirationDate = localStorage.getItem('expirationTime') || '0';
+
+  const remaingTime = calculateRemainingTime(+ storedExpirationDate);
+
+  if(remaingTime <= 1000) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationTime');
+    return null
+  }
+
+  return {
+    token: storedToken,
+    duration: remaingTime
+  }
+}
+
+// 회원가입 URL, POST 방식으로 호출하는 함수이다.
+export const signupActionHandler = (email: string, password: string, nickname: string) => {
+  const URL = '/auth/signup'
+  const signupObject = { email, password, nickname };
+
+  const response = POST(URL, signupObject, {});
+  return response;
+};
+
+
+// 로그인 URL 을 POST 방식으로 호출하는 함수이다.
+export const loginActionHandler = (email:string, password: string) => {
+  const URL = '/auth/login';
+  const loginObject = { email, password };
+  const response = POST(URL, loginObject, {});
+
+  return response;
+};
+
+// 로그아웃 시 발생하는 함수이며, LocalStorage 에 저장된 토큰과 만료시간을 제거.
+export const logoutActionHandler = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('expirationTime');
+};
+
+// 유저의 정보를 GET 방식으로 호출
+export const getUserActionHandler = (token:string) => {
+  const URL = '/member/me';
+  const response = GET(URL, createTokenHeader(token));
+  return response;
+}
+// 사용자가 닉네임을 수정했을 때 발생하는 액션으로 POST 방식으로 호출
+export const changeNicknameActionHandler = ( nickname:string, token: string) => {
+  const URL = '/member/nickname';
+  const changeNicknameObj = { nickname };
+  const response = POST(URL, changeNicknameObj, createTokenHeader(token));
+
+  return response;
+}
+// 비밀번호 변경 시 발생하는 메서드로 POST 방식으로 호출
+export const changePasswordActionHandler = (
+  exPassword: string,
+  newPassword: string,
+  token: string
+) => {
+  const URL = '/member/password';
+  const changePasswordObj = { exPassword, newPassword }
+  const response = POST(URL, changePasswordObj, createTokenHeader(token));
+  return response;
+}
---
+++ Kopmorning/src/main/reactts/src/auth/fetch-auth-action.tsx
@@ -0,0 +1,80 @@
+// GET, POST, PUT, DELETE 요청을 간편하게 호출
+import axios, { AxiosError,AxiosResponse }  from 'axios';
+
+type ServerError = { errorMessage: string };
+type LoginFailType = { status: number, error: string,};
+
+interface FetchData {
+  method: string,
+  url: string,
+  data? : {},
+  header : {},
+}
+// 주어진 메서드, URL, 데이터, 헤더를 사용해 비동기적으로 HTTP 요청을 처리하는 함수.
+const fetchAuth = async (fetchData: FetchData) => {
+  const method = fetchData.method;
+  const url = fetchData.url;
+  const data = fetchData.data;
+  const header = fetchData.header;
+
+// 요청처리
+  try {
+    const response:AxiosResponse<any, any> | false =
+    (method === 'get' && (await axios.get(url, header))) ||
+    (method === 'post' && (await axios.post(url, data, header))) ||
+    (method === 'put' && (await axios.put(url, data, header))) ||
+    (method === 'delete' && (await axios.delete(url, header))
+    );
+
+    if(response && response.data.error) {
+      console.log((response.data as LoginFailType).error);
+      alert("Wrong ID or Password");
+      return null;
+    }
+
+    if (!response) {
+      alert("false!");
+      return null;
+    }
+
+    return response;
+
+  } catch(err) {
+
+    if (axios.isAxiosError(err)) {
+      const serverError = err as AxiosError<ServerError>;
+      if (serverError && serverError.response) {
+        console.log(serverError.response.data);
+        alert("failed!");
+        return null;
+      }
+    }
+
+    console.log(err);
+    alert("failed!");
+    return null;
+  }
+
+}
+
+const GET = ( url:string, header:{} ) => {
+  const response = fetchAuth({ method: 'get', url, header });
+  return response;
+};
+
+const POST = ( url:string, data: {}, header:{}) => {
+  const response = fetchAuth({ method: 'post', url, data, header })
+  return response;
+};
+
+const PUT = async ( url:string, data: {}, header:{}) => {
+  const response = fetchAuth({ method: 'put', url, data, header });
+  return response;
+};
+
+const DELETE = async ( url:string, header:{} ) => {
+  const response = fetchAuth({ method: 'delete', url, header });
+  return response;
+};
+
+export { GET, POST, PUT, DELETE }
