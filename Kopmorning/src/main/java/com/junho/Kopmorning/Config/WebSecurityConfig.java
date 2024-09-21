61c98284e2851381f2545c0f363a3c2685684429
Leejunho <junho767@naver.com>
2024-09-21 21:34:57 +0900

    SpringBoot Admin Server 구축

--- Kopmorning/src/main/java/com/junho/Kopmorning/Config/WebSecurityConfig.java
+++ Kopmorning/src/main/java/com/junho/Kopmorning/Config/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import com.junho.Kopmorning.jwt.JwtAccessDeniedHandler;
 import com.junho.Kopmorning.jwt.JwtAuthenticationEntryPoint;
 import com.junho.Kopmorning.jwt.JwtFilter;
 import com.junho.Kopmorning.jwt.TokenProvider;
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
