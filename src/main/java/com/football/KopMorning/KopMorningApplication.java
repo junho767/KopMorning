3e14435c53d235d98d8ad4e0493b02e7b0f265b4
wnsgh <junho767@naver.com>
2025-02-03 19:24:53 +0900

    init

---
+++ src/main/java/com/football/KopMorning/KopMorningApplication.java
@@ -0,0 +1,13 @@
+package com.football.KopMorning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KopMorningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KopMorningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/football/KopMorning/KopMorningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.football.KopMorning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KopMorningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
