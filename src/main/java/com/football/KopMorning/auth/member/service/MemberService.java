46db8c1b2b895b0fab331d8e09b9fb4cbc64c8aa
wnsgh <junho767@naver.com>
2025-02-03 21:11:16 +0900

    feat: 회원가입 시 비밀번호 암호화하여 저장 구현

--- src/main/java/com/football/KopMorning/auth/member/service/MemberService.java
+++ src/main/java/com/football/KopMorning/auth/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.football.KopMorning.auth.member.service;
 import com.football.KopMorning.auth.member.domain.Member;
 import com.football.KopMorning.auth.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,13 +11,14 @@ import org.springframework.stereotype.Service;
 public class MemberService {
     private final MemberRepository memberRepository;
     private final RandNicknameService randNicknameService;
+    private final PasswordEncoder passwordEncoder;
 
     public Member join(String email, String password) {
         String randNickname = randNicknameService.randNickname();
 
         Member member = Member.builder()
                 .email(email)
-                .password(password)
+                .password(passwordEncoder.encode(password))
                 .nickName(randNickname)
                 .build();
 
